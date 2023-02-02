903391d280f7cc31d034b92e686d7f848af4e208
Uri Shaked <uri@urishaked.com>
2023-02-02 16:10:02 +0200

    ci: action to auto build and update the binaries

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ build() {
     echo $(pwd)/config/$APP_PATH
     docker run -e "IDF_TARGET=$CHIP" -v "$(pwd)/$TARGET_DIR:/output" -v "$(pwd)/cache/${IDF_VERSION}:/root/.ccache" \
       -v "$(pwd)/config/$APP_PATH:/config:ro" \
-      -it espressif/idf:$IDF_VERSION \
+      espressif/idf:$IDF_VERSION \
       /bin/bash -c "cd /opt/esp/idf && cd $APP_PATH && cp /config/* . && idf.py uf2 && \
         cp build/uf2.bin /output/firmware.uf2 && git rev-parse HEAD > /output/commit.txt" && \
     docker inspect --format='{{index .RepoDigests 0}}' espressif/idf:$IDF_VERSION > $TARGET_DIR/image.txt
