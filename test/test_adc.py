7fd1b9a4e1b5abe03b3efa5137d96a3b76fb7757
Uri Shaked <uri@urishaked.com>
2024-06-11 06:50:00 +0300

    test: adc test

---
+++ test/test_adc.py
@@ -0,0 +1,36 @@
+import subprocess
+
+import pytest
+
+expected_adc1_3 = "EXAMPLE: ADC1 Channel[3] Raw Data: 2050"
+expected_adc2_0 = "EXAMPLE: ADC2 Channel[0] Raw Data: 2050"
+
+expected_text = {
+    "esp32": expected_adc2_0,
+    "esp32s2": expected_adc2_0,
+    "esp32s3": expected_adc2_0,  # Currently broken
+    "esp32c3": expected_adc1_3,
+    "esp32c6": expected_adc1_3,
+    "esp32h2": expected_adc1_3,
+    "esp32p4": expected_adc1_3,  # ESP-IDF does not yet support ADC on the ESP32-P4
+}
+
+
+@pytest.mark.parametrize("chip", ["esp32", "esp32s2", "esp32c3", "esp32c6", "esp32h2"])
+def test_hello_world(chip: str):
+
+    # Run the Wokwi CLI
+    result = subprocess.run(
+        [
+            "wokwi-cli",
+            "--elf",
+            f"../bin/{chip}/idf/latest/examples/peripherals/adc/oneshot_read/firmware.uf2",
+            "--timeout",
+            "60000",
+            "--expect-text",
+            expected_text[chip],
+            "--diagram-file",
+            f"diagram.{chip}.json",
+        ]
+    )
+    assert result.returncode == 0
