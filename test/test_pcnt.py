53562a4a9ea3e149a22cea451c3ba055e752a6d8
Uri Shaked <uri@urishaked.com>
2024-06-14 00:02:20 +0300

    test: pcnt tests

---
+++ test/test_pcnt.py
@@ -0,0 +1,24 @@
+import os
+import subprocess
+
+import pytest
+
+
+@pytest.mark.parametrize("chip", ["esp32", "esp32s2", "esp32s3", "esp32c6", "esp32h2"])
+def test_pcnt(chip: str):
+
+    # Run the Wokwi CLI
+    result = subprocess.run(
+        [
+            "wokwi-cli",
+            "--elf",
+            f"../bin/{chip}/idf/latest/components/esp_driver_pcnt/test_apps/pulse_cnt/firmware.uf2",
+            "--timeout",
+            "5000",
+            "--scenario",
+            "test_pcnt.scenario.yaml",
+            "--diagram-file",
+            f"diagram.{chip}.json",
+        ]
+    )
+    assert result.returncode == 0
