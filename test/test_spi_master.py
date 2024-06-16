66b8846c666a41c133de8980401dd7130593525e
Uri Shaked <uri@urishaked.com>
2024-06-16 23:59:01 +0300

    test: spi master tests
    currently fail due to missing features in Wokwi, such as SPI Slave and DMA-Controlled Configurable Segmented Transfer

---
+++ test/test_spi_master.py
@@ -0,0 +1,25 @@
+import subprocess
+
+import pytest
+
+
+@pytest.mark.parametrize(
+    "chip", ["esp32", "esp32s2", "esp32s3", "esp32c3", "esp32c6", "esp32h2"]
+)
+def test_pcnt(chip: str):
+
+    # Run the Wokwi CLI
+    result = subprocess.run(
+        [
+            "wokwi-cli",
+            "--elf",
+            f"../bin/{chip}/idf/latest/components/esp_driver_spi/test_apps/master/firmware.uf2",
+            "--timeout",
+            "10000",
+            "--scenario",
+            "test_spi_master.scenario.yaml",
+            "--diagram-file",
+            f"diagram.{chip}.json",
+        ]
+    )
+    assert result.returncode == 0
