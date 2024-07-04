827e7c8553c56bf5219463c2df6e82ecf56aba49
Uri Shaked <uri@urishaked.com>
2024-07-04 12:41:53 +0300

    test: add gptimer tests
    currently only for esp32/s2/s3/c3

---
+++ test/test_gptimer.py
@@ -0,0 +1,23 @@
+import subprocess
+
+import pytest
+
+
+@pytest.mark.parametrize("chip", ["esp32", "esp32s2", "esp32s3", "esp32c3"])
+def test_gptimer(chip: str):
+
+    # Run the Wokwi CLI
+    result = subprocess.run(
+        [
+            "wokwi-cli",
+            "--elf",
+            f"../bin/{chip}/idf/latest/components/esp_driver_gptimer/test_apps/gptimer/firmware.uf2",
+            "--timeout",
+            "20000",
+            "--scenario",
+            "test_gptimer.scenario.yaml",
+            "--diagram-file",
+            f"gptimer/diagram.{chip}.json",
+        ]
+    )
+    assert result.returncode == 0
