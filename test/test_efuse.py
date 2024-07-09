59de2b6936a50467c985afad292bc6db13689eb6
Uri Shaked <uri@urishaked.com>
2024-07-09 16:10:58 +0300

    test(efuse): add efuse tests
    related: wokwi/wokwi-features#840

---
+++ test/test_efuse.py
@@ -0,0 +1,25 @@
+import subprocess
+
+import pytest
+
+
+@pytest.mark.parametrize(
+    "chip", ["esp32", "esp32s2", "esp32s3", "esp32c3", "esp32c6", "esp32h2", "esp32p4"]
+)
+def test_efuse(chip: str):
+
+    # Run the Wokwi CLI
+    result = subprocess.run(
+        [
+            "wokwi-cli",
+            "--elf",
+            f"../bin/{chip}/idf/latest/components/efuse/test_apps/firmware.uf2",
+            "--timeout",
+            "20000",
+            "--scenario",
+            "test_efuse.scenario.yaml",
+            "--diagram-file",
+            f"diagram.{chip}.json",
+        ]
+    )
+    assert result.returncode == 0
