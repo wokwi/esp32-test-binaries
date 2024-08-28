9ce4d6a4d49fa43e7c7474fb39a022eec80ea13d
Uri Shaked <uri@urishaked.com>
2024-08-28 15:25:48 +0300

    test(spi_master): increase C3/C6/H2 cpu frequency
    Fixes a timing issue that caused "SPI Master clk_source and divider accuracy" test case to fail

--- test/test_spi_master.py
+++ test/test_spi_master.py
@@ -7,11 +7,6 @@ import pytest
     "chip", ["esp32", "esp32s2", "esp32s3", "esp32c3", "esp32c6", "esp32h2"]
 )
 def test_spi_master(chip: str):
-
-    diagram_dir = (
-        "" if chip in ["esp32", "esp32s2", "esp32s3", "esp32c3", "esp32c6"] else "../"
-    )
-
     # Run the Wokwi CLI
     result = subprocess.run(
         [
@@ -23,7 +18,7 @@ def test_spi_master(chip: str):
             "--scenario",
             "../test_spi_master.scenario.yaml",
             "--diagram-file",
-            f"{diagram_dir}diagram.{chip}.json",
+            f"diagram.{chip}.json",
             "./spi_master",
         ]
     )
