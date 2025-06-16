e53af9e768667108dc129cafc82ce15f7d8e3228
Uri Shaked <uri@urishaked.com>
2025-06-16 17:41:55 +0300

    test(adc): add esp32p4

--- test/test_adc.py
+++ test/test_adc.py
@@ -12,12 +12,12 @@ expected_text = {
     "esp32c3": expected_adc1_3,
     "esp32c6": expected_adc1_3,
     "esp32h2": expected_adc1_3,
-    "esp32p4": expected_adc1_3,  # ESP-IDF does not yet support ADC on the ESP32-P4
+    "esp32p4": expected_adc2_0,
 }
 
 
 @pytest.mark.parametrize(
-    "chip", ["esp32", "esp32s2", "esp32s3", "esp32c3", "esp32c6", "esp32h2"]
+    "chip", ["esp32", "esp32s2", "esp32s3", "esp32c3", "esp32c6", "esp32h2", "esp32p4"]
 )
 def test_adc(chip: str):
 
