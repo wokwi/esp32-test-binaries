ef537fbbd4e886bd77d5c5c0f12292dccd6627a6
Uri Shaked <uri@urishaked.com>
2025-12-14 22:35:59 +0200

    test(adc): enable esp32c5

--- test/test_adc.py
+++ test/test_adc.py
@@ -10,6 +10,7 @@ expected_text = {
     "esp32s2": expected_adc2_0,
     "esp32s3": expected_adc2_0,
     "esp32c3": expected_adc1_3,
+    "esp32c5": expected_adc1_3,
     "esp32c6": expected_adc1_3,
     "esp32h2": expected_adc1_3,
     "esp32p4": expected_adc2_0,
@@ -17,7 +18,7 @@ expected_text = {
 
 
 @pytest.mark.parametrize(
-    "chip", ["esp32", "esp32s2", "esp32s3", "esp32c3", "esp32c6", "esp32h2", "esp32p4"]
+    "chip", ["esp32", "esp32s2", "esp32s3", "esp32c3", "esp32c5", "esp32c6", "esp32h2", "esp32p4"]
 )
 def test_adc(chip: str):
 
