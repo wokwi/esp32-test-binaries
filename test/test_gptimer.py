2f036d4a41e14681e1261c2119043458c009ee2c
Uri Shaked <uri@urishaked.com>
2026-01-14 14:49:34 +0200

    test(gptimer): enable c5 target

--- test/test_gptimer.py
+++ test/test_gptimer.py
@@ -3,7 +3,9 @@ import subprocess
 import pytest
 
 
-@pytest.mark.parametrize("chip", ["esp32", "esp32s2", "esp32s3", "esp32c3", "esp32c6", "esp32h2"])
+@pytest.mark.parametrize(
+    "chip", ["esp32", "esp32s2", "esp32s3", "esp32c3", "esp32c5", "esp32c6", "esp32h2"]
+)
 def test_gptimer(chip: str):
 
     # Run the Wokwi CLI
