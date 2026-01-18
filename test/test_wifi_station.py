fd06a07013227058b8965fb7a726103e25e3c3d7
Uri Shaked <uri@urishaked.com>
2026-01-18 08:50:43 +0200

    test(wifi_station): wifi station tests

---
+++ test/test_wifi_station.py
@@ -0,0 +1,24 @@
+import os
+import subprocess
+
+import pytest
+
+
+@pytest.mark.parametrize("chip", ["esp32", "esp32s2", "esp32s3", "esp32c3", "esp32c6"])
+def test_wifi_function(chip: str):
+
+    # Run the Wokwi CLI
+    result = subprocess.run(
+        [
+            "wokwi-cli",
+            "--elf",
+            f"../bin/{chip}/idf/latest/examples/wifi/getting_started/station/firmware.uf2",
+            "--timeout",
+            "5000",
+            "--expect-text",
+            "wifi station: connected to ap SSID:Wokwi-GUEST password:",
+            "--diagram-file",
+            f"diagram.{chip}.json",
+        ]
+    )
+    assert result.returncode == 0
