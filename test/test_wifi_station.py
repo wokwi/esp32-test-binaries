bb13f0b4aea91449247f2810dd1a50d3b4edf3fb
Uri Shaked <uri@urishaked.com>
2026-02-01 22:54:49 +0200

    test(wifi_station): now actually add the P4

--- test/test_wifi_station.py
+++ test/test_wifi_station.py
@@ -3,7 +3,7 @@ import subprocess
 import pytest
 
 
-@pytest.mark.parametrize("chip", ["esp32", "esp32s2", "esp32s3", "esp32c3", "esp32c6"])
+@pytest.mark.parametrize("chip", ["esp32", "esp32s2", "esp32s3", "esp32c3", "esp32c6", "esp32p4"])
 def test_wifi_function(chip: str):
 
     # Run the Wokwi CLI
