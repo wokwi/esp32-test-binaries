48f79155837762500432edc5a715e5a118b1c2d4
Uri Shaked <uri@urishaked.com>
2026-03-17 21:34:10 +0200

    test(crypto): add c5/c61/p4 to test matrix

--- test/test_crypto.py
+++ test/test_crypto.py
@@ -1,12 +1,16 @@
+import os
 import subprocess
 
 import pytest
 
 
 @pytest.mark.parametrize(
-    "chip", ["esp32", "esp32s2", "esp32s3", "esp32c3", "esp32c6", "esp32h2", "esp32p4"]
+    "chip", ["esp32", "esp32s2", "esp32s3", "esp32c3", "esp32c5", "esp32c6", "esp32c61", "esp32h2", "esp32p4"]
 )
 def test_crypto(chip: str):
+    diagram = f"crypto/diagram.{chip}.json"
+    if not os.path.exists(diagram):
+        diagram = f"diagram.{chip}.json"
 
     # Run the Wokwi CLI
     result = subprocess.run(
@@ -19,7 +23,7 @@ def test_crypto(chip: str):
             "--expect-text",
             "Tests finished, rc=0",
             "--diagram-file",
-            f"diagram.{chip}.json",
+            diagram,
         ]
     )
     assert result.returncode == 0
